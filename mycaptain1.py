c0b18e73a07d5111eab71529b2508f88d7c8ba91
Meghala1999 <64689307+Meghala1999@users.noreply.github.com>
2020-10-27 18:29:21 +0530

    created radius of a circle AND accept the file name
    created the  radius of a circle && 
    created the accept the file name from the user print of extension of that.

---
+++ mycaptain1.py
@@ -0,0 +1,14 @@
+
+from math import pi
+r=float(input("input the radius of circle:"))
+print("the area of the circle with radius 1.1" + "is;"+str(pi*r**2))
+
+
+
+filename = input("input the filename: ")
+f_extns = filename.split(".")
+print("the extension of the file is: "+repr(f_extns[-1]))
+
+
+
+
